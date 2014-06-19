f7248160b2dd0793d112a1a4940661e24c4e6e37
Hack Reactor Students <rgoldfinger@gmail.com>
2014-06-18 20:47:45 -0700

    implement enqueue and dequeue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,34 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
-  }
+    // debugger;
+    // console.log(this);
+    this.on('dequeue', function(modelObject) {
+    // remove model from queue
+      this.remove(modelObject);
+    // if song playing, play first song
+
+      console.log(modelObject);
+    });
+
+    this.on('add', function(modelObject) {
+      // add model to queue
+      console.log(modelObject);
+
+      // if no song playing, play first song
+
+    });
+  },
+
+  playFirst: function() {
+    this.models[0].play();
+  },
+
+
+
+  // endSong: function() {
+  //   this.models.shift();
+  //   this.playFirst();
+  // },
 
 });
--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -5,6 +5,16 @@ var PlayerView = Backbone.View.extend({
   // see: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML5_audio_and_video
   el: '<audio controls autoplay />',
 
+  events: {
+    'ended': function() {
+      this.model.dequeue();
+    },
+
+    'pause': function() {
+      console.log("play paused");
+    }
+  },
+
   initialize: function() {
   },
 
